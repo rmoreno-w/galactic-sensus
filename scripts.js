a1faa921b2296c547d494ec304c3cdab9db0d00c
Rodrigo Moreno <rmoreno.w@gmail.com>
2024-10-01 04:13:58 -0300

    Exibir dados de um planeta ao clicar em um botao de planeta

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-const planetList = document.getElementById('planet-list');
+const planetListUL = document.getElementById('planet-list');
+let modalElement = document.getElementById('modal');
+let planetListArray = [];
 
 async function getPlanets() {
     const planetsURL = 'https://swapi.dev/api/planets/';
@@ -7,12 +9,35 @@ async function getPlanets() {
     const result = await fetchResult.json();
 
     const planets = result.results;
-
     planets.forEach((planet) => {
         const planetButton = document.createElement('button');
         planetButton.innerHTML = planet.name;
-        planetList.appendChild(planetButton); // console.log(planet);
+        planetButton.addEventListener('click', openModal);
+
+        const listItem = document.createElement('li');
+        listItem.appendChild(planetButton);
+        planetListUL.appendChild(listItem); // console.log(planet);
+
+        planetListArray.push(planet);
     });
+}
+
+function openModal(event) {
+    let nameElement = document.getElementById('planet-name');
+    let populationElement = document.getElementById('planet-population');
+    let terrainElement = document.getElementById('planet-terrain');
+    let climateElement = document.getElementById('planet-climate');
+
+    const clickedPlanetName = event.target.innerHTML;
+
+    const planetInfo = planetListArray.find((planet) => planet.name == clickedPlanetName);
+
+    nameElement.innerHTML = planetInfo.name;
+    populationElement.innerHTML = planetInfo.population;
+    terrainElement.innerHTML = planetInfo.terrain;
+    climateElement.innerHTML = planetInfo.climate;
+
+    modalElement.showModal();
 }
 
 getPlanets();
